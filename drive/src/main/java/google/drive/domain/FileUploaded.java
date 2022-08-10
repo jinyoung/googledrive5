15342e6d9e9c29a39dd2e5174d1d0e6762fba41d
Jinyoung Jang <jyjang@uengine.org>
2022-08-10 12:22:25 +0900

    commit msg

---
+++ drive/src/main/java/google/drive/domain/FileUploaded.java
@@ -0,0 +1,28 @@
+package google.drive.domain;
+
+import google.drive.domain.*;
+import google.drive.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class FileUploaded extends AbstractEvent {
+
+    private Long id;
+    private String name;
+    private Long size;
+    private Date createdTime;
+    private String type;
+    private String ownerId;
+
+    public FileUploaded(File aggregate) {
+        super(aggregate);
+    }
+
+    public FileUploaded() {
+        super();
+    }
+    // keep
+
+}
